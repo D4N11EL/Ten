0dee7658bf2c24ca697d06b2dfda89fedd4ecfeb
D4N11EL <101025579+D4N11EL@users.noreply.github.com>
2023-07-07 20:53:42 -0500

    cambios
    cambios

--- src/main/java/com/example/tendencias/controller/PersonaController.java
+++ src/main/java/com/example/tendencias/controller/PersonaController.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.example.tendencias.controller;
+
 import java.util.List;
 import com.example.tendencias.service.PersonaServiceImpl;
 import com.repaso.repaso.model.Persona;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Danie
  */
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT,
+    RequestMethod.DELETE})
 @RequestMapping("/api/persona")
 public class PersonaController {
 
